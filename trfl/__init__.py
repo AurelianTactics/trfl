a0b1049e0263d87f5b9b5f0756609f0d8c11bbea
AurelianTactics <jamesdilorenzo@gmail.com>
2019-02-08 01:33:05 -0500

    Update __init__.py

--- trfl/__init__.py
+++ trfl/__init__.py
@@ -28,9 +28,9 @@ from trfl.discrete_policy_gradient_ops import discrete_policy_entropy_loss
 from trfl.discrete_policy_gradient_ops import discrete_policy_gradient
 from trfl.discrete_policy_gradient_ops import discrete_policy_gradient_loss
 from trfl.discrete_policy_gradient_ops import sequence_advantage_actor_critic_loss
-from trfl.dist_value_ops import categorical_dist_double_qlearning
-from trfl.dist_value_ops import categorical_dist_qlearning
-from trfl.dist_value_ops import categorical_dist_td_learning
+#from trfl.dist_value_ops import categorical_dist_double_qlearning
+#from trfl.dist_value_ops import categorical_dist_qlearning
+#from trfl.dist_value_ops import categorical_dist_td_learning
 from trfl.dpg_ops import dpg
 from trfl.indexing_ops import batched_index
 from trfl.periodic_ops import periodically
